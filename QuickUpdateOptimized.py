fd027e8d56d58ca395d027e6bea24c50f1747da4
Stephen Cassaro <srcassaro@gmail.com>
2018-03-14 14:06:41 -0700

    v0.2
    No changes here to the underlying process. The code however has been streamlined. Not sure why I did this but it was fun.

---
+++ QuickUpdateOptimized.py
@@ -0,0 +1,27 @@
+import telnetlib
+import paramiko
+import time
+MXKTelnet = telnetlib.Telnet(input("Enter the IP of your device: "))
+VersionNumber = input("What version do you want to upgrade to?: ")
+MXKTelnet.read_until(b"login:")
+MXKTelnet.write(b"admin\n")
+MXKTelnet.read_until(b"password:")
+MXKTelnet.write(b"zhone\n")
+MXKTelnet.read_until(b"zSH>")
+MXKTelnet.write(b"dir\r")
+time.sleep(1)
+DirArray = (MXKTelnet.read_very_eager().decode('ascii')).split()
+RTLUtilSSH = paramiko.SSHClient()
+RTLUtilSSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+RTLUtilSSH.connect('10.57.99.101', '22', 'mhiggins', 'zhone123')
+for i in range(len(DirArray) - 1):
+    if ((DirArray[i].find(".bin") > -1) & (DirArray[i].find(".bin_") < 0) & (DirArray[i].find(".bin.") < 0) & (DirArray[i].find("rom.bin") < 0)):
+        stdin,stdout,stderr = RTLUtilSSH.exec_command("cd /data/release/MXK_" + VersionNumber + "; find | grep " + DirArray[i])
+        FilePathResponse = ''.join(stdout.readlines())
+        FileDownloadString = "image download 10.57.99.101 /release/MXK_" + VersionNumber + FilePathResponse[1:].strip(('\n\r')) + " /card1/" + DirArray[i] + "\r"
+        if(len(FileDownloadString) > 80):
+            MXKTelnet.write(FileDownloadString.encode('ascii'))
+            MXKTelnet.read_until(b"zSH>")
+MXKTelnet.write(b"exit\r")
+RTLUtilSSH.close()
+MXKTelnet.close()
