35a36da4a12d663cd5fe226c7afd624dd872df27
Stephen Cassaro <srcassaro@gmail.com>
2018-03-14 16:01:32 -0700

    v0.3
    Support for MXK systems added. Tested and confirmed to work.

--- QuickUpdateOptimized.py
+++ QuickUpdateOptimized.py
@@ -14,14 +14,21 @@ DirArray = (MXKTelnet.read_very_eager().decode('ascii')).split()
 RTLUtilSSH = paramiko.SSHClient()
 RTLUtilSSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 RTLUtilSSH.connect('10.57.99.101', '22', 'mhiggins', 'zhone123')
+FlashString = ""
 for i in range(len(DirArray) - 1):
     if ((DirArray[i].find(".bin") > -1) & (DirArray[i].find(".bin_") < 0) & (DirArray[i].find(".bin.") < 0) & (DirArray[i].find("rom.bin") < 0)):
         stdin,stdout,stderr = RTLUtilSSH.exec_command("cd /data/release/MXK_" + VersionNumber + "; find | grep " + DirArray[i])
         FilePathResponse = ''.join(stdout.readlines())
         FileDownloadString = "image download 10.57.99.101 /release/MXK_" + VersionNumber + FilePathResponse[1:].strip(('\n\r')) + " /card1/" + DirArray[i] + "\r"
         if(len(FileDownloadString) > 80):
+            print(FileDownloadString)
             MXKTelnet.write(FileDownloadString.encode('ascii'))
             MXKTelnet.read_until(b"zSH>")
+        if(DirArray[i].find("mxup") > -1 and DirArray[i].find("raw.bin") > -1):
+            FlashString = "image flash /card1/mxup2tg8graw.bin 1 all\r"
+if(FlashString != ""):
+    MXKTelnet.write(FlashString.encode('ascii'))
+    print(FlashString)
 MXKTelnet.write(b"exit\r")
 RTLUtilSSH.close()
 MXKTelnet.close()
